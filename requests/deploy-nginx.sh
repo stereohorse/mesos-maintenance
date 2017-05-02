eb93cc8ca6d12cf5cdd75f02d370d1f1e5363fa8
Nguyen Viet <tnguen@alfabank.ru>
2017-05-02 18:07:24 +0300

    feat: wait for slave tasks migration

--- requests/deploy-nginx.sh
+++ requests/deploy-nginx.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 
-cat deploy-nginx.json | http PUT http://vm1:8080/v2/apps?force=true
+cat deploy-nginx-1.json | http PUT http://vm1:8080/v2/apps?force=true
+cat deploy-nginx-2.json | http PUT http://vm1:8080/v2/apps?force=true
